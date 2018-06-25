09cfcf49976cdd3bd2045bf267b79d571ffa2773
Guillermo Gutierrez <guillegr123@gmail.com>
2018-06-25 01:05:32 -0600

    Crear tarjetas de partidos por jornada.

--- QW.Frontend/src/pages/JornadaPage.vue
+++ QW.Frontend/src/pages/JornadaPage.vue
@@ -1,31 +1,11 @@
 <template>
   <v-ons-page>
     <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-4">
+      <div class="row" v-for="grupoPartidos in partidosAgrupados"  v-bind:todo="grupoPartidos" :key="grupoPartidos.id">
+        <div class="col-sm-12 col-md-6 col-lg-4" v-for="partido in grupoPartidos.partidos" :key="partido.idPartido">
           <v-ons-card>
             <div class="title">
               Awesome framework 1
-            </div>
-            <div class="content">
-              Test
-            </div>
-          </v-ons-card>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-4">
-          <v-ons-card>
-            <div class="title">
-              Awesome framework 2
-            </div>
-            <div class="content">
-              Test
-            </div>
-          </v-ons-card>
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-4">
-          <v-ons-card>
-            <div class="title">
-              Awesome framework 3
             </div>
             <div class="content">
               Test
@@ -41,8 +21,27 @@
 export default {
   name: 'jornada',
   props: [ 'partidos' ],
+  created () {
+    // Creando grupos de 3 partidos, para dibujar en filas
+    var cont = 0
+    var i = -1
+    this.partidos.forEach(p => {
+      if (cont % 3 === 0) {
+        i++
+        this.partidosAgrupados.push({
+          id: i,
+          partidos: [p]
+        })
+      } else {
+        this.partidosAgrupados[i].partidos.push(p)
+      }
+      cont++
+    })
+  },
   data () {
-
+    return {
+      partidosAgrupados: []
+    }
   }
 }
 </script>
